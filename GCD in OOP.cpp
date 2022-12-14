4b9ec525138be8044ff4c00f260dda285f50c332
AnnaAstakolunts <120597489+AnnaAstakolunts@users.noreply.github.com>
2022-12-15 03:10:48 +0400

    Add files via upload

---
+++ GCD in OOP.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class GCD {
+private:
+	int a, b;
+public:
+	GCD(int A, int B)
+	{
+		a = A;
+		b = B;
+	}
+	void function()
+	{
+		if (a == 0 || b == 0)
+			cout << 0 << endl;
+		while (a != b)
+		{
+			if (a < b)
+			{
+				b = b - a;
+			}
+			else
+			{
+				a = a - b;
+			}
+		}
+		cout << b << endl;
+}
+};
+
+
+int main()
+{
+	GCD obj(196, 14);
+	cout << "The great common divisor is: ";
+	obj.function();
+
+		return 0;
+}
+
+
+
+
+ 
